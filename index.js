b31ab3aecd915f1c981539d0df1a755f9a6aba35
asandamhlongo <156101659+asandamhlongo@users.noreply.github.com>
2024-04-01 22:31:15 +0200

    Added js code to make the app functional

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
-const result = document.getElementById("#result");
-const sound = document.getElementById("#sound");
-const btn = document.getElementById("#search-btn");
-
-btn.addEventListener("click", () => {
-    let inputWord = document.getElementById("#input-word").value;
-    console.log(inputWord);
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+const btn = document.getElementById("search-btn");
+btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    let inputWord = document.getElementById("input-word").value;
+    fetch(`${url}${inputWord}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            result.innerHTML = `
+            <div class="word">
+                    <h3>${inputWord}</h3>
+                    <button onclick="playSound()">
+                        <box-icon type='solid' name='volume-full'></box-icon>
+                    </button>
+                </div>
+                <div class="details">
+                    <p>${data[0].meanings[0].partOfSpeech}</p>
+                    <p>/${data[0].phonetic}/</p>
+                </div>
+                <p class="word-meaning">
+                   ${data[0].meanings[0].definitions[0].definition}
+                </p>
+                <p class="word-example">
+                    ${data[0].meanings[0].definitions[0].example || ""}
+                </p>`;
+            sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
+        })
+        .catch(() => {
+            result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
+        });
 });
+function playSound() {
+    sound.play();
+}
+
